591e975e31e8e7624af9a4187a323ea4e026ebfc
Daniel Rotman <danilarothmann@gmail.com>
2025-11-06 16:33:05 +0700

    minimum working version v1 add new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.danielrothmann.dr"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.danielrothmann.dr"
@@ -28,12 +28,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -52,4 +52,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Koin для Android
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.navigation) // опционально для Navigation
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.fragment.ktx)
 }
